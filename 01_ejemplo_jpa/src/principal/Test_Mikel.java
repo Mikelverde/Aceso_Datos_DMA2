771f62b6d8f08b363c9d51ae676c1669ad295b58
usuario <usuario@Win7-base.localdomain>
2020-02-17 16:35:08 +0100

    PDF02 Terminado

---
+++ 01_ejemplo_jpa/src/principal/Test_Mikel.java
@@ -0,0 +1,38 @@
+package principal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import entidades.Contacto;
+
+public class Test_Mikel {
+
+	public static void main(String[] args) {
+		//CREAMOSUN ENTITYMANAGER
+		EntityManagerFactory fac=Persistence.createEntityManagerFactory("01_ejemplo_jpa");
+		EntityManager em=fac.createEntityManager();
+		//INICIAMOS LA TRANSACCION
+		EntityTransaction tx=em.getTransaction();
+		tx.begin();
+		//PERSISTO, CREO UNA ENTIDAD
+		Contacto contacto= new Contacto();
+		contacto.setIdContactos(1);
+		contacto.setNombre("Mikel");
+		contacto.setEmail("mikelcanfranc@gmail.com");
+		contacto.setTelefono(639737507);
+		em.merge(contacto);
+		//ACTUALIZAMOS UNA ENTIDAD
+		contacto=em.find(Contacto.class, 1);
+		contacto.setEmail("modificado2@gmail.com");
+		em.merge(contacto);
+		//ELIMINAMOS UN CONTACTO
+		contacto=em.find(Contacto.class, 1);
+		em.remove(contacto);
+		System.out.println("OPERACIONES REALIZADAS");
+		tx.commit();
+		em.close();
+	}//FIN MAIN
+
+}//FIN CLASE TEST_MIKEL
